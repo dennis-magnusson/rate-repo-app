803a39cc983fcb04f203518574e0efeb3e40407e
Dennis Magnusson <magnden@gmail.com>
2022-09-27 16:09:46 +0300

    10.7 completed

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import Constants from 'expo-constants';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Link } from 'react-router-native';
 
 const styles = StyleSheet.create({
@@ -17,19 +17,22 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     paddingBottom: 15,
     paddingTop: 15,
-    paddingLeft: 15
+    paddingLeft: 10,
+    paddingRight: 10
   }
 });
 
 const AppBar = () => {
   return(
   <View style={styles.container}>
+    <ScrollView horizontal>
         <Link to="/">
           <View><Text style={styles.headingText}>Repositories</Text></View>
         </Link>
         <Link to="/signin">
           <View><Text style={styles.headingText}>Sign in</Text></View>
         </Link>
+    </ScrollView>
   </View>);
 };
 
